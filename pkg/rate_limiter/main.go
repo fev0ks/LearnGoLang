4e39426cd3c06a600369ecb04afc5c8d5ce668ff
Mikhail Patin <mikhail.patin@netcracker.com>
2024-01-27 16:15:42 +0600

    Updates

--- pkg/rate_limiter/main.go
+++ pkg/rate_limiter/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"strconv"
 	"sync"
+	"time"
 
 	"github.com/sirupsen/logrus"
 )
@@ -28,8 +29,8 @@ func (c client) SendRequest(ctx context.Context, request Request) error {
 func main() {
 	ctx := context.Background()
 	c := client{}
-	requests := make([]Request, 100)
-	for i := 0; i < 100; i++ {
+	requests := make([]Request, 1000)
+	for i := 0; i < 1000; i++ {
 		requests[i] = Request{Payload: strconv.Itoa(i)}
 	}
 	log := logrus.New()
@@ -40,12 +41,14 @@ func main() {
 // 1. In fly requests limit
 // 2. Per second limit
 func makeBatchApiCalls(ctx context.Context, c Client, log *logrus.Logger, requests []Request) {
+	rateLimiter := newRateLimiter(100)
 	wg := sync.WaitGroup{}
 	for _, r := range requests {
 		r := r
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
+			rateLimiter.wait()
 			err := c.SendRequest(ctx, r)
 			if err != nil {
 				log.WithError(err).Error("send request")
@@ -53,4 +56,30 @@ func makeBatchApiCalls(ctx context.Context, c Client, log *logrus.Logger, reques
 		}()
 	}
 	wg.Wait()
+}
+
+type inFlyLimiter struct {
+	pool chan []struct{}
+}
+
+func newInFlyLimiter(poolSize int) *inFlyLimiter {
+	return &inFlyLimiter{pool: make(chan []struct{}, poolSize)}
+}
+
+func (ifl *inFlyLimiter) wait() {
+
+}
+
+type rateLimiter struct {
+	ch *time.Ticker
+}
+
+func (rl *rateLimiter) wait() {
+	<-rl.ch.C
+}
+
+func newRateLimiter(rps int) *rateLimiter {
+	interval := time.Second / time.Duration(rps)
+	tck := time.NewTicker(interval)
+	return &rateLimiter{ch: tck}
 }
